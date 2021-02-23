f1229f65d2a4fb98e475c73cab1da5085dbe0fa8
Anton Sizikov <sizikov.anton@gmail.com>
2021-02-23 15:31:16 +0100

    Add two projects: GameManager and Player

---
+++ src/CloudEng.PingPong.GameManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CloudEng.PingPong.GameManager
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/CloudEng.PingPong.Player/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CloudEng.PingPong.Player.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(ILogger<GameController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
