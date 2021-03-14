299f147f1cfbfc5b22c11d1006800f25b8c03dcb
Anton Sizikov <sizikov.anton@gmail.com>
2021-03-14 20:23:56 +0100

    fix unit tests

---
+++ src/CloudEng.PingPong.Player/Configuration/PlayerConfigOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace CloudEng.PingPong.Player.Configuration
+{
+    public class PlayerConfigOptions
+    {
+        public string PlayerName { get; set; }
+        public string OpponentName { get; set; }
+    }
+}
--- src/CloudEng.PingPong.Player/Controllers/IPlayerStateManager.cs
+++ src/CloudEng.PingPong.Player/Controllers/IPlayerStateManager.cs
@@ -6,6 +6,7 @@ namespace CloudEng.PingPong.Player.Controllers
     public interface IPlayerStateManager
     {
         Task SetStateAsync(State state, CancellationToken cancellationToken);
+        Task SetStateAsync(State state, int counter, CancellationToken cancellationToken);
         Task<PlayerState> GetStateAsync(CancellationToken cancellationToken);
     }
 }
